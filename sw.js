1e3071f9717757fe0ed264f9d42ff6f938a0dc35
magicboy0409 <magic123kid@gmail.com>
2025-02-09 15:13:51 +0800

    Site updated: 2025-02-09 15:13:51

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1739082281306";
+const VERSION = "1739085225748";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
